6ddd88c47d92d87437609ca8a0491a844c45075b
mattjedge <matt.jj.edge@gmail.com>
2025-02-01 22:41:30 +0000

    Test coverage for UpdateQuality quality

--- csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
@@ -7,11 +7,87 @@ namespace GildedRoseTests;
 public class GildedRoseTest
 {
     [Test]
-    public void Foo()
+    public void GildedRose_has_list_of_items()
     {
         var items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
         var app = new GildedRose(items);
         app.UpdateQuality();
-        Assert.That(items[0].Name, Is.EqualTo("fixme"));
+        Assert.That(items[0].Name, Is.EqualTo("foo"));
+    }
+
+    [Test]
+    public void UpdateQuality_degrades_standard_item_quality()
+    {
+        var items = new List<Item> { new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 } };
+        var app = new GildedRose(items);
+        app.UpdateQuality();
+        Assert.That(items[0].Quality, Is.EqualTo(19));
+    }
+
+    [Test]
+    public void UpdateQuality_double_degrades_standard_item_quality_after_SellIn_reached()
+    {
+        var items = new List<Item> { new Item { Name = "+5 Dexterity Vest", SellIn = 0, Quality = 20 } };
+        var app = new GildedRose(items);
+        app.UpdateQuality();
+        Assert.That(items[0].Quality, Is.EqualTo(18));
+    }
+
+    [Test]
+    public void UpdateQuality_cannot_reduce_item_quality_below_zero()
+    {
+        var items = new List<Item> { new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 0 } };
+        var app = new GildedRose(items);
+        app.UpdateQuality();
+        Assert.That(items[0].Quality, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void AgedBrie_increases_item_quality_over_time()
+    {
+        var items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 10, Quality = 0 } };
+        var app = new GildedRose(items);
+        app.UpdateQuality();
+        Assert.That(items[0].Quality, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void UpdateQuality_cannot_increase_item_quality_above_50()
+    {
+        var items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 10, Quality = 50 } };
+        var app = new GildedRose(items);
+        app.UpdateQuality();
+        Assert.That(items[0].Quality, Is.EqualTo(50));
+    }
+
+    [Test]
+    public void UpdateQuality_does_not_increase_Sulfuras_quality()
+    {
+        var items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 } };
+        var app = new GildedRose(items);
+        app.UpdateQuality();
+        Assert.That(items[0].Quality, Is.EqualTo(80));
+    }
+
+    [TestCase(11, 8)]
+    [TestCase(10, 9)]
+    [TestCase(5, 10)]
+    [TestCase(0, 0)]
+    public void Backstage_pass_increases_in_value_as_concert_approaches(int sellIn, int quality)
+    {
+        var items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = sellIn, Quality = 7 } };
+        var app = new GildedRose(items);
+        app.UpdateQuality();
+        Assert.That(items[0].Quality, Is.EqualTo(quality));
+    }
+
+    // boo
+    [Test]
+    public void UpdateQuality_passes_time()
+    {
+        var items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 7 } };
+        var app = new GildedRose(items);
+        app.UpdateQuality();
+        Assert.That(items[0].SellIn, Is.EqualTo(4));
     }
 }
