26e84d601387ee39ad7ba952f496203a823804ed
mattjedge <matt.jj.edge@gmail.com>
2025-02-02 00:23:19 +0000

    Adapter refactor

--- csharp.NUnit/GildedRose/GildedRose.cs
+++ csharp.NUnit/GildedRose/GildedRose.cs
@@ -2,82 +2,156 @@
 
 namespace GildedRoseKata;
 
+public interface IItemQualityAdapter
+{
+    public void ApplyQualityRules();
+    public void UpdateSellIn();
+}
+
+public class NormalItemAdapter : IItemQualityAdapter
+{
+    private readonly Item _item;
+
+    public NormalItemAdapter(Item item)
+    {
+        _item = item;
+    }
+
+    public void UpdateSellIn()
+    {
+        _item.SellIn -= 1;
+    }
+
+    public void ApplyQualityRules()
+    {
+        if (_item.Quality > 0)
+        {
+            _item.Quality -= 1;
+        }
+        if (_item.Quality > 0 && _item.SellIn <= 0)
+        {
+            _item.Quality -= 1;
+        }
+    }
+}
+
+public class LegendaryItemAdapter : IItemQualityAdapter
+{
+    private readonly Item _item;
+    public LegendaryItemAdapter(Item item)
+    {
+        _item = item;
+    }
+
+    public void ApplyQualityRules()
+    {
+        // No degradation for Legendary items
+    }
+
+    public void UpdateSellIn()
+    {
+        // Always desired
+    }
+}
+
+public class AgedItemAdapter : IItemQualityAdapter
+{
+    private readonly Item _item;
+    public AgedItemAdapter(Item item)
+    {
+        _item = item;
+    }
+
+    public void ApplyQualityRules()
+    {
+        if (_item.Quality < 50)
+        {
+            _item.Quality += 1;
+        }
+    }
+
+    public void UpdateSellIn()
+    {
+        _item.SellIn -= 1;
+    }
+}
+
+public class TimeSensitiveItemAdapter : IItemQualityAdapter
+{
+    private readonly Item _item;
+    public TimeSensitiveItemAdapter(Item item)
+    {
+        _item = item;
+    }
+
+    public void ApplyQualityRules()
+    {
+        if (_item.Quality < 50)
+        {
+            _item.Quality += 1;
+        }
+        if (_item.Quality < 50 && _item.SellIn < 11)
+        {
+            _item.Quality += 1;
+        }
+        if (_item.Quality < 50 && _item.SellIn < 6)
+        {
+            _item.Quality += 1;
+        }
+        if (    _item.SellIn <= 0)
+        {
+            _item.Quality = 0;
+        }
+    }
+
+    public void UpdateSellIn()
+    {
+        _item.SellIn -= 1;
+    }
+}
+
+
 public class GildedRose
 {
     IList<Item> Items;
+    IDictionary<Item, IItemQualityAdapter> _itemDictionary;
 
     public GildedRose(IList<Item> Items)
     {
         this.Items = Items;
+        _itemDictionary = new Dictionary<Item, IItemQualityAdapter>();
+        foreach (var item in Items)
+        {
+            _itemDictionary[item] = AdapterFor(item);
+        }
+    }
+
+    public IItemQualityAdapter AdapterFor(Item item)
+    {
+        if (item.Name == "Sulfuras, Hand of Ragnaros")
+        {
+            return new LegendaryItemAdapter(item);
+        }
+        else if (item.Name == "Aged Brie")
+        {
+            return new AgedItemAdapter(item);
+        }
+        else if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+        {
+            return new TimeSensitiveItemAdapter(item);
+        }
+        else
+        {
+            return new NormalItemAdapter(item);
+        }
     }
 
     public void UpdateQuality()
     {
-        foreach (var item in Items)
+        foreach (var item in _itemDictionary)
         {
-            if (item.Name == "Sulfuras, Hand of Ragnaros")
-            {
-                continue;
-            }
-            else if (item.Name == "Aged Brie")
-            {
-                IncreaseQualityToMaxOf50(item);
-                DecreaseSellIn(item);
-                continue;
-            }
-            else if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-            {
-                UpcomingEventQualityApplier(item);
-                continue;
-            }
-            else
-            {
-                DecreaseQualityToMinOf0(item);                
-                DecreaseSellIn(item);
-            }
-        }
-    }
-
-    private void UpcomingEventQualityApplier(Item item)
-    {
-        IncreaseQualityToMaxOf50(item);
-        if (item.SellIn < 11)
-        {
-            IncreaseQualityToMaxOf50(item);
-        }
-        if (item.SellIn < 6)
-        {
-            IncreaseQualityToMaxOf50(item);
-        }
-        if (item.SellIn <= 0)
-        {
-            item.Quality = 0;
-        }
-        DecreaseSellIn(item);
-    }
-
-    private void DecreaseSellIn(Item item)
-    {
-        item.SellIn -= 1;
-    }
-
-    private void DecreaseQualityToMinOf0(Item item)
-    {
-        if (item.Quality > 0)
-        {
-            item.Quality -= 1;
-        }
-        if (item.SellIn <= 0)
-        {
-            item.Quality -= 1;
-        }
-    }
-
-    private void IncreaseQualityToMaxOf50(Item item)
-    {
-        if (item.Quality < 50)
-        {
-            item.Quality += 1;
-        }
+            item.Value.ApplyQualityRules();
+            item.Value.UpdateSellIn();
+        };
     }
 }
